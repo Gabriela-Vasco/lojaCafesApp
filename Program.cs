f6ff3081a5a2ba9bb3e2932a7c6f855f71f2c4fc
Gabriela <gabrielavs1226@gmail.com>
2023-11-21 22:27:11 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddSingleton<ICafeService, CafeService>();
+builder.Services.AddTransient<ICafeService, CafeService>();
 
 builder.Services.AddDbContext<DataContext>();
 
